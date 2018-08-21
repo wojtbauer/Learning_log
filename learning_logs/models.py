f7b0c7bb0276112b118bb3423d4a5d10b37311d7
wojtbauer <wojciech.brk1@gmail.com>
2018-08-21 11:32:41 +0200

    Update#6

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """Temat poznawany przez użytkownika"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         """Zwraca reprezentacje modelu w postaci cigu liczbowego"""
         return self.text
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,19 +10,27 @@ def index(request):
     """Strona główna dla aplikacji learning_log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Wyświetnie wszystkich tematów"""
     topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Wyświetla pojedynczy temat i wszystkie powiązane z nim wpisy"""
     topic = Topic.objects.get(id=topic_id)
+    #Upewniamy sie że temat należy do zalogowanego użytkownika
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Dodaj nowy temat"""
     if request.method != "POST":
@@ -31,12 +40,15 @@ def new_topic(request):
         """Przekazano dane za pomocą żądania POST, należy je powtórzyć"""
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))        
             
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
-    
+
+@login_required    
 def new_entry(request, topic_id):
     """Dodawanie nowego wpisu dla określonego tematu."""
     topic = Topic.objects.get(id=topic_id)
@@ -57,11 +69,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edycja istniejącego wpisu"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    
+    if topic.owner != request.user:
+        raise Http404
     if request.method != "POST":
         #Żądanie początkowe, wypełnienie formularza aktualną treścią wpisu
         form = EntryForm(instance=entry)
